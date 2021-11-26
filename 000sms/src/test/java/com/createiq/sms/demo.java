f620717174cf90885c10d938602075a2992bbcc8
sures <sures@LAPTOP-UNNEGM3M>
2021-11-26 12:54:09 +0530

    my first commit

---
+++ 000sms/src/test/java/com/createiq/sms/demo.java
@@ -0,0 +1,20 @@
+package com.createiq.sms;
+
+import org.testng.annotations.Test;
+
+
+public class demo {
+	
+	@Test
+	
+	public void test() {
+		System.out.println("trst 1");
+//		System.out.println("trst 1");
+//		System.out.println("trst 1");
+//		System.out.println("trst 1");
+//		System.out.println("trst 1");
+//		System.out.println("trst 1");
+//		System.out.println("trst 1");
+	}
+
+}
